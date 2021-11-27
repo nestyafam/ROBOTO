59c676d7fa2adf16e6ccdf34fec99f4aa094a98d
uwadiekene <nestyafam@gmail.com>
2021-11-27 01:05:06 -0500

    Project Roboto

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "2fwDQKF8bG15iof0XGETBL0HfSxeIGdP"
 
-    MONGODB_SETTINGS = {'db': 'Project_Roboto'}
-
-
+    MONGODB_SETTINGS = {'db'    : 'Project_Roboto',
+                        'host'  : 'localhost',
+                        'port'  :27017
+    }
